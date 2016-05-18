2b2b6c93cf87ea8ca53b4e127ee3c733ffbc8f22
spela_pecnik <pecnik.spela@gmail.com>
2016-05-18 12:01:28 +0200

    Interface IKomitent

--- SpletnaBankaJPA/ejbModule/ejb/IKomitent.java
+++ SpletnaBankaJPA/ejbModule/ejb/IKomitent.java
@@ -1,5 +1,12 @@
 package ejb;
 
-public interface IKomitent {
+import javax.ejb.Local;
 
+import entitete.Komitent;
+
+@Local
+public interface IKomitent {
+	void shrani(Komitent k);
+	void izbrisi(Komitent k);
+	Komitent najdi(Komitent k);
 }
