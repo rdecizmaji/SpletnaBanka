ef05bf359624d00d962f50f7cb5ca446568361e5
Luka Horvatic <luka.horvatic@yahoo.co.uk>
2016-06-01 09:04:43 +0200

    popravil typo

--- SpletnaBankaJPA/ejbModule/ejb/TransakcijskiRacunEJB.java
+++ SpletnaBankaJPA/ejbModule/ejb/TransakcijskiRacunEJB.java
@@ -27,7 +27,7 @@ public class TransakcijskiRacunEJB implements ITransakcijskiRacun {
 
 	@Override
 	public void shrani(TransakcijskiRacun trr) {
-		trr.setDatumOdprja(Calendar.getInstance());
+		trr.setDatumOdprtja(Calendar.getInstance());
 		em.persist(trr);
 		System.out.println("Transakcijski racun shranjen.");
 	}
